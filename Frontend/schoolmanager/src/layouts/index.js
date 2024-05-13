9094252e1f170c845373ab0768e12a8070c42326
ngduchao <duchao0202@gmail.com>
2024-05-13 15:14:49 +0700

    update FE 13/5

--- Frontend/schoolmanager/src/layouts/index.js
+++
@@ -1,3 +0,0 @@
-export { default as DefaultLayout } from "./DefaultLayout/DefaultLayout";
-export { default as ManagerLayout } from "./ManagerLayout/ManagerLayout";
-export { default as ErrorPageLayout } from "./ErrorPageLayout/ErrorPageLayout";
--- Frontend/schoolmanager/src/pages/ErrorPage/Page404/Page404.js
+++
@@ -1,9 +0,0 @@
-function Page404() {
-    return (
-        <div>
-            <h1>404 Error Page</h1>
-        </div>
-    );
-}
-
-export default Page404;
